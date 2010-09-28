c96f5edc74c10c079e695d713b48fe80d8fc96ed
mole <mole@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2010-09-28 15:18:15 +0000

    ISSUE: PURPOSE:  Preparing for release to Specify team. DESCRIPTION: Adding GPL2+ for specify consistency, per James.

--- htdocs/php_includes/populate_web_tables.php
+++ htdocs/php_includes/populate_web_tables.php
@@ -7,6 +7,24 @@
  * 
  * Created on Aug 24, 2010
  * @author Paul J. Morris
+ * 
+ * Copyright © 2010 President and Fellows of Harvard College
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ * @Author: Paul J. Morris  bdim@oeb.harvard.edu
  *
  *The following grants will be needed:
    grant select  on specify.* to 'specify_web_admi'@'localhost' identified by 'password';
