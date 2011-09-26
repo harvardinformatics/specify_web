d9418b724dba75abfa0403752a66fa7bca0d85a0
mole <mole@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2011-09-26 19:50:11 +0000

    ISSUE: BugID: 520 PURPOSE: From Julie, provide a means to list types per family more rapidly than current stats web page. DESCRIPTION: Added queries on web_search to iterate through families and get numbers of type specimens per family.

--- htdocs/htdocs/stats.php
+++ htdocs/htdocs/stats.php
@@ -40,6 +40,9 @@ if ($_GET['mode']!="")  {
 	if ($_GET['mode']=="family_type_count") {
 		$mode = "family_type_count"; 
 	}
+	if ($_GET['mode']=="family_type_count_summary") {
+		$mode = "family_type_count_summary"; 
+	}
 } 
 	
 echo pageheader('qc'); 
@@ -54,6 +57,9 @@ if (preg_match("/^140\.247\.98\./",$_SERVER['REMOTE_ADDR']) || $_SERVER['REMOTE_
 		switch ($mode) {
 		    case "family_type_count":
 		        echo family_type_count();
+		        break;
+		    case "family_type_count_summary":
+		        echo family_type_count_summary();
 		        break;
 			case "menu": 	
 			default:
@@ -88,6 +94,7 @@ function menu() {
    $returnvalue .= "<h2>Type Counts</h2>";
    $returnvalue .= "<ul>";
    $returnvalue .= "<li><a href='stats.php?mode=family_type_count'>Counts of number of types by family (slow)</a></li>";
+   $returnvalue .= "<li><a href='stats.php?mode=family_type_count_summary'>Counts of number of types by family (from web search cache)</a></li>";
    $returnvalue .= "</ul>";
    $returnvalue .= "</div>";
 
@@ -112,6 +119,37 @@ function family_type_count() {
                     $query2 = "select count(distinct fragment.identifier) from taxon left join determination on taxon.taxonid = determination.taxonid left join fragment on determination.fragmentid = fragment.fragmentid where nodenumber >= ? and highestchildnodenumber <= ? and determination.typestatusname is not null and determination.typestatusname <> 'NotType';";
 	            $statement2 = $connection->prepare($query2);
                     $statement2->bind_param("ii",$nodenumber,$highestchildnodenumber);
+		    $statement2->bind_result($count);
+		    $statement2->execute();
+		    $statement2->store_result();
+		    while ($statement2->fetch()) {
+	                $returnvalue .= "<td>$count</td>";
+		    }
+	            $returnvalue .= "</tr>";
+                }
+	        $returnvalue .= "</table>";
+	}
+
+   return $returnvalue;
+}
+function family_type_count_summary() { 
+	global $connection;
+   $returnvalue = "";
+    
+   $query = "select distinct family from web_search where family is not null order by family ";
+   if ($debug) { echo "[$query]<BR>"; } 
+      $returnvalue .= "<h2>Counts of the number of specimens in each family that have a type status (other than just 'NotType').  Counts calculated from the web search cache table, may be slightly different than counts calculated from current data.</h2>";
+	$statement = $connection->prepare($query);
+	if ($statement) {
+		$statement->execute();
+		$statement->bind_result($family);
+		$statement->store_result();
+	        $returnvalue .= "<table>";
+		while ($statement->fetch()) {
+	            $returnvalue .= "<tr><td>$family</td>";
+                    $query2 = "select count(distinct barcode) from web_search where typestatus is not null and typestatus <> 'NotType' and family = ? ;";
+	            $statement2 = $connection->prepare($query2);
+                    $statement2->bind_param("s",$family);
 		    $statement2->bind_result($count);
 		    $statement2->execute();
 		    $statement2->store_result();
