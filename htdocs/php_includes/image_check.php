1e94e6afe79c42d4473303a1521632885a57a85c
mole <mole@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2012-05-18 16:03:05 +0000

    Script to put list of image files into database.

---
+++ htdocs/php_includes/image_check.php
@@ -0,0 +1,92 @@
+<?php
+
+// scans one or more directory trees and places metadata about files
+// found matching LAPI/GPI filenameing convention into a database table
+// defined by: 
+// create table IMAGE_LOCAL_FILE (id bigint not null primary key auto_increment, base text,  path text, filename varchar(900), extension varchar(4), barcode varchar(10), mimetype varchar(255), fragmentid bigint);
+
+
+
+$debug=TRUE;
+
+$basedirectories[0] = "/private/var/automount/nfs_reshares/share_root-1/";
+
+include_once('connection_library.php');
+include_once('specify_library.php');
+
+$connection = specify_connect();
+$errormessage = "";
+
+$mode = "rebuild";
+
+$PREFIXPATTERN = "^(GH|AMES|ECON|FH|A|NEBC)";  // note, contains () affects array index in $matches
+$BARCODEPATTERN = "([0-9]{8})";  // note, contains () affects array index in $matches
+$EXTENSIONPATTERN = "\.([Tt][Ii][Ff]{1,2})$"; // note, contains () affects array index in $matches
+$SUFFIXPATTERN = "(_[a-z]){0,1}"; // note, contains () affects array index in $matches
+
+if ($connection) {
+
+for ($x=0;$x<count($basedirectories);$x++) { 
+   checkDirectory($basedirectories[$x], $basedirectories[$x]);
+}
+
+}
+
+function checkDirectory($base, $path)  {
+global $debug;
+ 
+   if ($debug) { echo "PATH: $path\n"; } 
+
+   @$files = scandir($path);
+   for ($i=0; $i<count($files); $i++) { 
+     if ($files[$i]!='.' && $files[$i]!='..') { 
+        if (is_dir($path . '/' . $files[$i])) { 
+          checkDirectory($base, $path . '/' . $files[$i]);
+        } else { 
+          checkFile($base, $path,$files[$i]);
+        }
+     }
+   } 
+
+} 
+
+function checkFile($base, $path,$filename) { 
+global $debug, $connection, $PREFIXPATTERN, $BARCODEPATTERN, $SUFFIXPATTERN, $EXTENSIONPATTERN;
+
+   $pathbelowbase = substr($path,strlen($base));
+
+   if ($debug) { echo "$pathbelowbase $filename"; } 
+
+   if (preg_match("/$PREFIXPATTERN$BARCODEPATTERN$SUFFIXPATTERN$EXTENSIONPATTERN/",$filename,$matches)) { 
+
+       $barcode = $matches[2]; // note, index affected by patterns that contain ()
+       $extension = $matches[4]; // note, index affected by patterns that contain ()
+       $mimetype = mime_content_type($path.'/'.$filename);
+       if ($debug) { echo " matched $barcode $extension $mimetype "; } 
+
+       $sql = "select fragmentid from fragment where identifier = ? ";
+        $statement = $connection->prepare($sql);
+        if ($statement) {
+           $statement->bind_param("s",$barcode);
+           $statement->execute();
+           $statement->bind_result($fragmentid);
+           $statement->store_result();
+           while ($statement->fetch()) {
+               if ($debug) { echo "$fragmentid"; } 
+               $insert = "insert into IMAGE_LOCAL_FILE (fragmentid,base,path,filename,barcode,extension,mimetype) values ('$fragmentid','$base','$pathbelowbase','$filename','$barcode','$extension','$mimetype')";
+               $statement2 = $connection->prepare($insert);
+               if ($statement2) {
+                   // $statement->bind_param("s",$barcode);
+                   $statement2->execute();
+                   $statement2->close();
+               }
+               echo $connection->error;
+           } // end statement fetch
+        } // end if statement
+   } // end match filename
+
+   if ($debug) { echo "\n"; } 
+
+}
+ 
+?>
