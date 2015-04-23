fb3143afb5ad1cff3fc24b1341bfbcef7dfc9ea3
mole <mole@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2015-04-23 17:02:04 +0000

    ISSUE: PURPOSE: Adding recapcha to reduce spam to Kanchi.  DESCRIPTION: Adding the current google reCaptcha into the comment and correction forms, and processor for it into mail.php.  Anne Marie obtained the API key for us to use with the google account that is used for web stats.

--- htdocs/htdocs/mail.php
+++ htdocs/htdocs/mail.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once("ReCaptcha.php");
+
 $add_correct_addressee = "gandhi@oeb.harvard.edu";
 $comment_addressee = "cms-comments@oeb.harvard.edu";
 $smtp_address = "smtp.lsdiv.harvard.edu";
@@ -7,50 +9,64 @@ $init_address = "huh_it@oeb.harvard.edu";
 
 $reply = "Submission received.  Thank you";
 
+
 $reqtype = preg_replace("[^a-z]","",$_POST['reqtype']);
 $name = preg_replace("[^A-Z\ a-z::alpha::\.]","",$_POST['name']);
 $replyTo = preg_replace("[^A-Za-z0-9\.\@_\-~#]","",$_POST['email']);
 $msgtext = $_POST['usertext'];
 $msgtext = trim($msgtext);
 
-if ($name=="") { $name = "no name given"; } 
-if ($reqtype!="add") { $reqtype = "comment"; } 
+$gRecaptchaResponse = $_POST['g-recaptcha-response'];
+$recaptcha = new ReCaptcha('6LcDywUTAAAAAGr4rLkqhyMyGLQdSinNH-fU4c6P');
+$recaptresponse = $recaptcha->verify($gRecaptchaResponse, $_SERVER['REMOTE_ADDR']);
+if ($recaptresponse->isSuccess()) {
 
-$subject = "";
-$to = "";	    
-if ($reqtype == "add") {
-    $to = $add_correct_addressee;
-    $subject = "Addition/correction submission via CMS website";
-} else { 
-    $to = $comment_addressee;
-    $subject = "Comment submission via CMS website";
-}
-	    
-$body = "The following message was submitted to the HUH website by " . $name .". \n";
-	   
-$replyTo = $replyTo.trim(); 
-$replyHeader = "";
-if ($replyTo!="") {
-    $body .= "Please use ". $replyTo ." to respond to the sender.\r\n\r\n";
-    $replyHeader = "Reply-To: ". $replyTo . "\r\n" ;
+   if ($name=="") { $name = "no name given"; } 
+   if ($reqtype!="add") { $reqtype = "comment"; } 
+   
+   $subject = "";
+   $to = "";	    
+   if ($reqtype == "add") {
+       $to = $add_correct_addressee;
+       $subject = "Addition/correction submission via CMS website";
+   } else { 
+       $to = $comment_addressee;
+       $subject = "Comment submission via CMS website";
+   }
+   	    
+   $body = "The following message was submitted to the HUH website by " . $name .". \n";
+   	   
+   $replyTo = $replyTo.trim(); 
+   $replyHeader = "";
+   if ($replyTo!="") {
+       $body .= "Please use ". $replyTo ." to respond to the sender.\r\n\r\n";
+       $replyHeader = "Reply-To: ". $replyTo . "\r\n" ;
+   } else {
+       $body .= "The sender did not include an email address.\r\n\r\n";
+   }
+   
+   $body .= $msgtext;
+   $headers = 'From: ' . $init_address  . "\r\n" .
+              $replyHeader .
+              'X-Mailer: PHP/' . phpversion();
+   	    
+   if (($msgtext!="")) {
+       // uncomment the following line to test
+       // $to = "bdim@oeb.harvard.edu";
+       $result = mail($to,$subject,$body,$headers);
+       if (!$result) {
+           $reply = "Error sending message.";
+       }
+   } else { 
+       $reply = "No message content.";
+   }
+
 } else {
-    $body .= "The sender did not include an email address.\r\n\r\n";
-}
-
-$body .= $msgtext;
-$headers = 'From: ' . $init_address  . "\r\n" .
-           $replyHeader .
-           'X-Mailer: PHP/' . phpversion();
-	    
-if (($msgtext!="")) {
-    // uncomment the following line to test
-    // $to = "bdim@oeb.harvard.edu";
-    $result = mail($to,$subject,$body,$headers);
-    if (!$result) {
-        $reply = "Error sending message.";
-    }
-} else { 
-    $reply = "No message content.";
+    $errors = $recaptresponse->getErrorCodes();
+    $reply = "Error generating message.";
+    foreach($errors as $error) { 
+      $reply .= " $error ";
+    } 
 }
 
 $majorContext = "Databases";
@@ -101,7 +117,6 @@ echo $reply;
 
 <?php
 
-echo pagefooter();
+echo pagefooter('specimen');
 
 ?>
-
