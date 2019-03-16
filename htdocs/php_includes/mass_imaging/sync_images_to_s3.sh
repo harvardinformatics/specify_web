069f49e1cf4ff9bf02864bfc4b6cd5c2609770fc
Jonathan Kennedy <jonathan_kennedy@harvard.edu>
2019-03-16 10:57:34 -0400

    Fixed syntax

--- htdocs/php_includes/mass_imaging/sync_images_to_s3.sh
+++ htdocs/php_includes/mass_imaging/sync_images_to_s3.sh
@@ -4,7 +4,7 @@
 content=$(cat test.txt)
 if [ "Symlinks working!" != "$content" ];then
     echo "ERROR: huhimagestorage/ is not mounted with mfsymlinks"
-    exit(1)
+    exit 1
 fi
 
 # run the sync - excluding CR2 and DNG for now
