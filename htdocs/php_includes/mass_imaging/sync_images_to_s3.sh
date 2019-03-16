8f9f90d603981577e3fdb58064ea4a2639cdd85f
Jonathan Kennedy <jonathan_kennedy@harvard.edu>
2019-03-16 10:49:20 -0400

    Script for syncing images to S3

---
+++ htdocs/php_includes/mass_imaging/sync_images_to_s3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make sure that the filesystem is mounted correctly with mfsymlinks (CIFS only?)
+content=$(cat test.txt)
+if [ "Symlinks working!" != "$content" ];then
+    echo "ERROR: huhimagestorage/ is not mounted with mfsymlinks"
+    exit(1)
+fi
+
+# run the sync - excluding CR2 and DNG for now
+aws s3 sync --exclude "*.dng" --exclude "*.CR2" /mnt/huhimagestorage/huhspecimenimages/ s3://huhspecimenimages/
