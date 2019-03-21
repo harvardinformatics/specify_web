93c923815028c152fc3fb4121babead3ebd9de44
Jonathan Kennedy <jonathan_kennedy@harvard.edu>
2019-03-20 20:10:52 -0400

    Only show one IMAGE_OBJECT per IMAGE_SET

--- htdocs/htdocs/databases/specimen_search.php
+++ htdocs/htdocs/databases/specimen_search.php
@@ -382,6 +382,7 @@ function details() {
 								" left join REPOSITORY r on o.repository_id = r.id " .
 								" left join IMAGE_OBJECT_TYPE t on o.object_type_id = t.id " .
 								" where c.collectionobjectid = ? and hidden_flag = 0 and active_flag = 1 " .
+								" group by o.image_set_id, t.name " .
 								" order by o.image_set_id, left(right(o.object_name,5),1), object_type_id desc ";
 							if ($debug===true) {  echo "[$query]<BR>"; }
 							$statement_img = $connection->prepare($query);
