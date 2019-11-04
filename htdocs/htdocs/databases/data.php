1fd17263aab55ca86fea17b054bad26b640f4d42
Jonathan Kennedy <jonathan_kennedy@harvard.edu>
2019-11-04 11:31:34 -0500

    Adding preview image link

--- htdocs/htdocs/databases/data.php
+++ htdocs/htdocs/databases/data.php
@@ -3,30 +3,30 @@
  * Created on Feb 16, 2019
  *
  * Copyright 2019 The President and Fellows of Harvard College
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * 
+ *
+ *
  */
 $debug=false;
 
 include_once('connection_library.php');
 include_once('specify_library.php');
 
-if ($debug) { 
+if ($debug) {
 	mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT);
-} else { 
+} else {
 	mysqli_report(MYSQLI_REPORT_OFF);
 }
 
@@ -80,6 +80,8 @@ switch ($mode) {
 
 	case "image":
 		redirectImage($collectionobjectid);
+	case "preview":
+		redirectPreview($collectionobjectid);
 	case "thumb":
 		redirectThumbnail($collectionobjectid);
 	case "rdf":
@@ -92,16 +94,16 @@ die();
 
 function redirectImage($coid) {
 	global $connection;
-	$query=" 
+	$query="
 select concat(r.url_prefix, io.uri) url
 from IMAGE_SET_collectionobject isc,
      IMAGE_OBJECT io,
      REPOSITORY r
-where 
+where
 	isc.imagesetid = io.image_set_id and
 	io.repository_id = r.id and
 	io.object_type_id = 4 and
-	io.active_flag = 1 and 
+	io.active_flag = 1 and
 	isc.collectionobjectid =".$coid;
 
 	$url = "empty";
@@ -115,30 +117,67 @@ where
 		$statement->close();
 	} else {
 		printError("System error");
-	}	
-	
+	}
+
 	if (strcmp($url,"empty") == 0) {
 		send404();
 		die();
 	}
-	
+
 	header("Location: ".$url);
 	die();
-	
+
+}
+
+function redirectPreview($coid) {
+	global $connection;
+	$query="
+select concat(r.url_prefix, io.uri) url
+from IMAGE_SET_collectionobject isc,
+     IMAGE_OBJECT io,
+     REPOSITORY r
+where
+	isc.imagesetid = io.image_set_id and
+	io.repository_id = r.id and
+	io.object_type_id = 3 and
+	io.active_flag = 1 and
+	isc.collectionobjectid =".$coid;
+
+	$url = "empty";
+
+	$statement = $connection->prepare($query);
+	if ($statement) {
+		$statement->execute();
+		$statement->bind_result($url);
+		$statement->store_result();
+		$statement->fetch();
+		$statement->close();
+	} else {
+		printError("System error");
+	}
+
+	if (strcmp($url,"empty") == 0) {
+		send404();
+		die();
+	}
+
+	header("Location: ".$url);
+	die();
+
 }
 
 function redirectThumbnail($coid) {
 	global $connection;
-	$query=" 
+	$query="
 select concat(r.url_prefix, io.uri) url
 from IMAGE_SET_collectionobject isc,
      IMAGE_OBJECT io,
      REPOSITORY r
-where 
+where
 	isc.imagesetid = io.image_set_id and
 	io.repository_id = r.id and
 	io.object_type_id = 2 and
-	io.active_flag = 1 and 
+	io.active_flag = 1 and
 	isc.collectionobjectid =".$coid;
 
 	$url = "empty";
@@ -152,16 +191,16 @@ where
 		$statement->close();
 	} else {
 		printError("System error");
-	}	
-	
+	}
+
 	if (strcmp($url,"empty") == 0) {
 		send404();
 		die();
 	}
-	
+
 	header("Location: ".$url);
 	die();
-	
+
 }
 
 function redirectRDF($u) {
@@ -170,13 +209,13 @@ function redirectRDF($u) {
 	die();
 }
 
-function redirectPage($coid) {	
+function redirectPage($coid) {
 	$url = "http://data.huh.harvard.edu/databases/specimen_search.php?mode=details&id=".$coid;
 	header("Location: ".$url);
-	die();	
+	die();
 }
 
-function printError($text) { 
+function printError($text) {
 	echo "<strong>Error: $text</strong>";
 	die();
 }
@@ -188,6 +227,5 @@ function send404() {
 }
 
 mysqli_report(MYSQLI_REPORT_OFF);
- 
-?>
 
+?>
