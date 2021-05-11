b26eb0e8dd0a73e3777750d0ba9553ab8c9ec548
jakennedy <jonathan_kennedy@harvard.edu>
2021-05-10 20:02:50 -0400

    Update ingest_images.sh

--- htdocs/php_includes/mass_imaging/ingest_images.sh
+++ htdocs/php_includes/mass_imaging/ingest_images.sh
@@ -155,7 +155,7 @@ for d in $BASE_DIR ; do # iterate through the directories for each photostation
   							linkfile=$(./link_image.sh "$target" "$IMG_DIR/JPG-Preview" "nobc$r" "jpg") || { echo "ERROR: link_image.sh failed for nobc$r ($linkfile)" ; exit 1; }
                 echo "linked $target to $linkfile"
   							aws s3 cp --no-progress --quiet "$linkfile" "$S3DIR/JPG/" &
-  							php copy_image_object.php "$jpgid" "$linkfile" 1 "" || { echo "ERROR: copy_image_object.php failed for $linkfile" ; exit 1; }
+  							php copy_image_object.php "$jpgpid" "$linkfile" 1 "" || { echo "ERROR: copy_image_object.php failed for $linkfile" ; exit 1; }
             fi
 
 					done
