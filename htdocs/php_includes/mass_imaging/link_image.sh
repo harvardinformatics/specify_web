c8764c58adb6e1b134c7926f5a509ec9793db619
jakennedy <jonathan_kennedy@harvard.edu>
2022-09-25 22:58:11 -0400

    Update link_image.sh

--- htdocs/php_includes/mass_imaging/link_image.sh
+++ htdocs/php_includes/mass_imaging/link_image.sh
@@ -11,15 +11,15 @@ barcode=$3
 ext=$4
 linkfile="$linkdir/$barcode.$ext"
 
-if [ -f "$linkfile" ] ; then
-	
+if [[ -L "$linkfile" || -f "$linkfile" ]] ; then
+
 	if [ "$linkfile" -ef "$sourcefile" ] ; then
 		#echo "WARN: symlink already exists at $linkfile for $sourcefile"
 		:
 	else
 		for x in {a..z} ; do
 			linkfile="$linkdir/${barcode}_$x.$ext"
-			if [ -f "$linkfile" ] ; then
+			if [[ -L "$linkfile" || -f "$linkfile" ]] ; then
 				if [ "$linkfile" -ef "$sourcefile" ] ; then
 					#echo "WARN: symlink already exists at $linkfile for $sourcefile"
 					break
@@ -27,29 +27,29 @@ if [ -f "$linkfile" ] ; then
 					continue
 				fi
 			else
-			
+
 				ln -sr "$sourcefile" "$linkfile"
-				
+
 				if [ $? != 0 ] ; then
 					echo "ERROR: Symlink failed for $linkfile to $sourcefile"
 					exit 1;
-				fi	
-				
+				fi
+
 				#echo "Linked $linkfile to $sourcefile"
-				break			
-			fi									
+				break
+			fi
 		done
 
 	fi
 else
 
 	ln -sr "$sourcefile" "$linkfile"
-	
+
 	if [ $? != 0 ] ; then
 		echo "ERROR: Symlink failed for $linkfile to $sourcefile"
 		exit 1;
-	fi		
-	
+	fi
+
 	#echo "Linked $linkfile to $sourcefile"
 fi
 
